366bc2630587d3d7dc4617728239002bf9594134
sana <mnvnarasimhulu4@gmail.com>
2025-09-06 19:26:42 +0530

    Added Form UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Welcome to the Form Page</h1>
+      <form>
+        <label>
+          Name:
+          <input type="text" name="name" />
+        </label><br /><br />
+        <label>
+          Email:
+          <input type="email" name="email" />
+        </label><br /><br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
 
-export default App;
+export default App;
